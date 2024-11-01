753c11f6c3b30c18dd8778692711ae9f0fefa26d
Riza <rizahan.yusuf@ist.id>
2024-11-01 15:11:50 +0700

    update: version update
    - update css
    - update npm version

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,23 +1,38 @@
 // components/Header.tsx
 
 import Link from "next/link";
+import Image from "next/image"; // Import the Image component
 import { Navigation } from "../constant/navigation/navigation";
 
 const Header: React.FC = () => {
   return (
     <header className="bg-gray-900 text-white py-4">
       <div className="container mx-auto flex justify-between items-center px-4">
-        <Link href="/">MONOKROM</Link>
+        {/* Make the logo clickable */}
+        <Link href="/" className="flex items-center">
+          <Image
+            className="dark:invert"
+            src="../assets/monokrom_logo.svg"
+            alt="MONOKROM logo"
+            width={256}
+            height={6}
+            priority
+          />
+        </Link>
+
         <nav>
           <ul className="flex space-x-6">
             {Navigation.items.map((item) => (
-              <Link
-                key={item.href}
-                href={item.href}
-                className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
-              >
-                {item.name}
-              </Link>
+              <li key={item.href}>
+                {" "}
+                {/* Wrap link in a list item */}
+                <Link
+                  href={item.href}
+                  className="text-sm text-zinc-500 hover:text-zinc-300 transition duration-300"
+                >
+                  {item.name}
+                </Link>
+              </li>
             ))}
           </ul>
         </nav>
