283ddefc98481ce14a24ecae1dd3cfb6af46e634
Riza <rizahan.yusuf@ist.id>
2024-11-08 17:00:07 +0700

    move: refactoring

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -1,75 +1,104 @@
-"use client";
 import Image from "next/image";
 import Link from "next/link";
 import Header from "../../src/components/header";
-import Logo from "/public/images/monokrom.png";
-import Profile from "../../src/components/profile_card";
 
-export default function Home() {
+export default function AboutUs() {
   return (
-    <main className="flex min-h-screen flex-col">
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <div className="flex flex-col items-center justify-center min-h-screen bg-white">
-        <h1 className="text-4xl font-bold mb-4">Your Business, Digitalized.</h1>
-        <p className="text-gray-600 text-lg mb-8">
-          The best place to begin the digital existence of your business.
-        </p>
-        <button className="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800 transition-colors duration-300">
-          Get Started
-        </button>
-      </div>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-white">
-        <h1 className="text-4xl font-bold mb-4">Our Clients.</h1>
-        <p className="text-gray-600 text-lg mb-8">
-          The best place to begin the digital existence of your business.
-        </p>
-        <div className="flex items-center flex-col gap-6 justify-center sm:flex-row">
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
           <Image
-            className="light:invert"
-            src={Logo}
-            alt="MONOKROM logo"
-            width={256}
-            height={61}
+            src="https://nextjs.org/icons/next.svg"
+            alt="Next.js logo"
+            width={180}
+            height={38}
+            priority
           />
-          <Image
-            className="light:invert"
-            src={Logo}
-            alt="MONOKROM logo"
-            width={256}
-            height={61}
-          />
-          <Image
-            className="light:invert"
-            src={Logo}
-            alt="MONOKROM logo"
-            width={256}
-            height={61}
-          />
-        </div>
+          <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+            <li className="mb-2">
+              Get started by editing{" "}
+              <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+                src/app/page.tsx
+              </code>
+              .
+            </li>
+            <li>MONOKROM</li>
+          </ol>
+
+          <div className="flex gap-4 items-center flex-col sm:flex-row">
+            <a
+              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                className="dark:invert"
+                src="https://nextjs.org/icons/vercel.svg"
+                alt="Vercel logomark"
+                width={20}
+                height={20}
+              />
+              Deploy now
+            </a>
+            <a
+              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Read our docs
+            </a>
+          </div>
+        </main>
+        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+          <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="https://nextjs.org/icons/file.svg"
+              alt="File icon"
+              width={16}
+              height={16}
+            />
+            Learn
+          </a>
+          <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="https://nextjs.org/icons/window.svg"
+              alt="Window icon"
+              width={16}
+              height={16}
+            />
+            Examples
+          </a>
+          <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="https://nextjs.org/icons/globe.svg"
+              alt="Globe icon"
+              width={16}
+              height={16}
+            />
+            Go to nextjs.org →
+          </a>
+        </footer>
       </div>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-white">
-        <h1 className="text-4xl font-bold mb-4">Testimonials.</h1>
-        <p className="text-gray-600 text-lg mb-8">
-          Read what others have to say.
-        </p>
-        <div className="flex items-center flex-col gap-6 justify-center sm:flex-row">
-          <Profile
-            imageSrc="/images/36.jpg" // replace with your image URL
-            quote="Fully committed! We take responsibility in mentoring and fulfilling our commitments."
-            author="Mr. Abdurrahman"
-          />
-          <Profile
-            imageSrc="/images/36.jpg" // replace with your image URL
-            quote="Fully committed! We take responsibility in mentoring and fulfilling our commitments."
-            author="Mr. Abdurrahman"
-          />
-          <Profile
-            imageSrc="/images/36.jpg" // replace with your image URL
-            quote="Fully committed! We take responsibility in mentoring and fulfilling our commitments."
-            author="Mr. Abdurrahman"
-          />
-        </div>
-      </div>
-    </main>
+    </div>
   );
 }
