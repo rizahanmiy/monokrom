23a35b631eda81719c425f4c2182e24d35e6599c
Riza <rizahan.yusuf@ist.id>
2024-11-13 07:21:47 +0700

    add: contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,6 +3,7 @@ import { Github, Mail, Twitter } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../../src/components/nav";
 import { Card } from "../../src/components/card";
+import Header from "@/components/header";
 
 const socials = [
   {
@@ -28,7 +29,7 @@ const socials = [
 export default function Example() {
   return (
     <div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
-      <Navigation />
+      <Header />
       <div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
         <div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
           {socials.map((s) => (
